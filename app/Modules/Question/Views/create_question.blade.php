cd0b91640c477b6ee5d3956477864f3cb60e0157
Tran Minh Quan <tmq1201@gmail.com>
2017-08-10 15:48:08 +0700

    add create question

---
+++ app/Modules/Question/Views/create_question.blade.php
@@ -0,0 +1,51 @@
+@extends('Core::template.index')
+@section('content')
+<div class="col-md-12">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Question</h3>
+        </div>
+        <div class="panel-body">
+            {!! Form::open(['url' => 'question/detail/{id}/create']) !!}
+                <div class="form-group">
+                    {!! Form::label('question', 'Question') !!}
+                    {!! Form::text('question', '', ['class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('option_0', 'A') !!}
+                    {!! Form::text('option_0', '', ['class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('option_1', 'B') !!}
+                    {!! Form::text('option_1', '', ['class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('option_2', 'C') !!}
+                    {!! Form::text('option_2', '', ['class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('option_3', 'D') !!}
+                    {!! Form::text('option_3', '', ['class' => 'form-control', 'required' => 'required']) !!}
+                </div>
+                <input type='text' value={{$id}} name='bank_id' style="display:none;">
+                <input type='text' value={{$id}} name='answer' style="display:none;">
+                <button class="btn btn-success" type="submit">Add new question</button>
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
+
+
+
+@stop
+@section('slider')
+   <section id="imgBanner">
+       <h2>Add Question</h2>
+   </section>
+
+
+@stop
+
+@section('sidebar')
+@include('Core::template.sidebar')
+@stop
